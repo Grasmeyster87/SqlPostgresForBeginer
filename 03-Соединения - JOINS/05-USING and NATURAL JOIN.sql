c1814a8581df5e802cf7971460ba4888195fcf70
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2024-12-11 20:20:37 +0200

    ERROR -ON- USING

--- 03-Соединения - JOINS/05-USING and NATURAL JOIN.sql
+++ 03-Соединения - JOINS/05-USING and NATURAL JOIN.sql
@@ -11,9 +11,9 @@ SELECT contact_name, company_name, phone, first_name, last_name, title,
 	order_date, product_name, ship_country, products.unit_price, quantity, discount
 FROM orders
 JOIN order_details USING(order_id)
-JOIN products ON USING(product_id)
-JOIN customers ON USING(customer_id)
-JOIN employees ON USING(employee_id)
+JOIN products USING(product_id)
+JOIN customers USING(customer_id)
+JOIN employees USING(employee_id)
 WHERE ship_country = 'USA';
 
 SELECT order_id, customer_id, first_name, last_name, title
